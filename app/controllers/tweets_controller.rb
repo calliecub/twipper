f74ab3351e9e2fc3f3c6c65e375566613c871b2b
Callie Leone <callie.leone11@gmail.com>
2014-11-30 16:17:34 -0500

    added functionality to create tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,27 @@
 class TweetsController < ApplicationController
 	def new
+		@tweet = Tweet.new
 	end
-	
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		#params
+		if @tweet.save
+			flash.now[:success] = "Tweet succesfully created"
+			#do something
+		else
+			flash.now[:danger] = "Tweet cannot be saved.  Please fix errors below."
+			#do something else
+		end
+
+		render 'new'
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:message)
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
     root 'static_pages#main_page'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+    resources :tweets
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
